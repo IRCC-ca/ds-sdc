c7cb398bb7e17e8fcc91fdb55e903d8432ce06b9
Claire <clairebun_3@hotmail.com>
2022-04-11 13:04:14 -0400

    update ng config to build and update project name

--- sample/src/app/app.module.ts
+++ sample/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
 
-import { JlTemplateModule, JlCommonModule } from '@ircc-ca/ds-sdc-angular';
+import { JlTemplateModule, JlCommonModule } from '@ircc-ca/ds-sdc-angular'
 
 @NgModule({
   declarations: [
