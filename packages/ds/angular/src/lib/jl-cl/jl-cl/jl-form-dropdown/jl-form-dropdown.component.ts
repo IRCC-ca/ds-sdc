70ba65d9dcf56cf8d908f504ac10fa4303807333
Claire Bun <clairebun_3@hotmail.com>
2022-04-11 13:22:26 -0400

    Package our NG Library to be consummed (#55)
    * added button component
    
    * remove jl-cl lib and update configs to call pkg
    
    * update ng config to build and update project name
    
    Co-authored-by: Sarah Harvey <101356948+harvey-sarah@users.noreply.github.com>

--- packages/ds/angular/src/lib/jl-cl/jl-cl/jl-form-dropdown/jl-form-dropdown.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { ListFieldComponent } from '../jl-list-field';
-
-@Component({
-    selector: 'jlcl-dropdown',
-    templateUrl: './jl-form-dropdown.component.html',
-    styleUrls: ['./jl-form-dropdown.component.scss'],
-})
-export class DynDropDownComponent extends ListFieldComponent {}
