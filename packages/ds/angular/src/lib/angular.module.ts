b8f806f6e0efe6403eb5a030d4db600e029b5ea6
Claire <clairebun_3@hotmail.com>
2022-04-12 13:56:46 -0400

    Linting, unit test and format fixes

--- packages/ds/angular/src/lib/angular.module.ts
+++ packages/ds/angular/src/lib/angular.module.ts
@@ -2,14 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AngularComponent } from './angular.component';
 @NgModule({
-  declarations: [
-    AngularComponent,
-  ],
-  imports: [
-    CommonModule,
-  ],
-  exports: [
-    AngularComponent
-  ]
+    declarations: [AngularComponent],
+    imports: [CommonModule],
+    exports: [AngularComponent],
 })
 export class AngularModule {}
