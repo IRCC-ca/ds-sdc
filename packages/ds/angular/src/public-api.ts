7302a7f341a5bbdcbc7a5b07262f9d4b16f5229d
harvey-sarah <101356948+harvey-sarah@users.noreply.github.com>
2022-04-21 15:40:41 -0400

    added link component, minor code tidy-up (#64)
    * added link component, minor code tidy-up
    
    * linting fixes

--- packages/ds/angular/src/public-api.ts
+++ packages/ds/angular/src/public-api.ts
@@ -3,14 +3,18 @@
  */
 
 // DIRECTIVES
+export * from './common/button/button.component';
+export * from './common/icon/icon.component';
+export * from './common/jl-common.module';
+export * from './common/link/link.component';
 export * from './stencil-generated/components';
 export * from './stencil-generated/index';
+export * from './template/footer/footer.component';
+export * from './template/header/header.component';
+export * from './template/jl-template.module';
 // import * as d from './stencil-generated/components';
 // export const DIRECTIVES = [
 //   d.MyComponent
 // ];
 // PACKAGE MODULE
 export { IRCCModule } from './web-module';
-
-
-
