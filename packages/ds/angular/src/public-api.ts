1d87ac5df58b360d751070448a4eb6c8f6107bc1
Hannah.Cass <92103482+cass-h@users.noreply.github.com>
2022-04-22 09:15:45 -0400

    rework of stencil/angular integration

--- packages/ds/angular/src/public-api.ts
+++ packages/ds/angular/src/public-api.ts
@@ -4,14 +4,13 @@
 
 // DIRECTIVES
 export * from './common/button/button.component';
+export * from './common/footer/footer.component';
+export * from './common/header/header.component';
 export * from './common/icon/icon.component';
 export * from './common/jl-common.module';
 export * from './common/link/link.component';
 export * from './stencil-generated/components';
 export * from './stencil-generated/index';
-export * from './template/footer/footer.component';
-export * from './template/header/header.component';
-export * from './template/jl-template.module';
 // import * as d from './stencil-generated/components';
 // export const DIRECTIVES = [
 //   d.MyComponent
