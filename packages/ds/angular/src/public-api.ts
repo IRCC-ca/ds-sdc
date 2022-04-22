7212c1d54a9134c44ac4a70365c75e0b77c4035b
Hannah.Cass <92103482+cass-h@users.noreply.github.com>
2022-04-22 11:17:28 -0400

    delete comments

--- packages/ds/angular/src/public-api.ts
+++ packages/ds/angular/src/public-api.ts
@@ -11,9 +11,6 @@ export * from './common/jl-common.module';
 export * from './common/link/link.component';
 export * from './stencil-generated/components';
 export * from './stencil-generated/index';
-// import * as d from './stencil-generated/components';
-// export const DIRECTIVES = [
-//   d.MyComponent
-// ];
 // PACKAGE MODULE
 export { IRCCModule } from './web-module';
+
