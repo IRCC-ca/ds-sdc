914f04552d7683cd122740f2d2df6eec387c5c13
Bun.Claire <claire.bun@cic.gc.ca>
2022-05-02 15:58:53 -0400

    add checkbox and radiobutton components

--- packages/ds/angular/src/common/jl-common.module.ts
+++ packages/ds/angular/src/common/jl-common.module.ts
@@ -4,25 +4,29 @@ import { IconComponent } from './icon/icon.component';
 import { ButtonComponent } from './button/button.component';
 import { LinkComponent } from './link/link.component';
 import { HeaderComponent } from './header/header.component'
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { RadioButtonComponent } from './radio-button/radio-button.component';
+import { CheckboxComponent } from './checkbox/checkbox.component'
+
+export const LIST_COMPONENTS = [
+    IconComponent,
+    ButtonComponent,
+    LinkComponent,
+    HeaderComponent,
+    FooterComponent,
+    RadioButtonComponent,
+    CheckboxComponent
+]
 
 @NgModule({
     declarations: [
-        IconComponent,
-        ButtonComponent,
-        LinkComponent,
-        HeaderComponent,
-        FooterComponent
+        LIST_COMPONENTS
     ],
     imports: [
         CommonModule
     ],
     exports: [
-        IconComponent,
-        ButtonComponent,
-        LinkComponent,
-        HeaderComponent,
-        FooterComponent
+        LIST_COMPONENTS
     ],
     providers: [],
 })
---
+++ packages/ds/angular/src/common/radio-button/radio-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jds-radio-button',
+  templateUrl: './radio-button.component.html',
+  styleUrls: ['./radio-button.component.css']
+})
+export class RadioButtonComponent  { }
