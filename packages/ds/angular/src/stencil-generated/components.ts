9f08f4e7c668d26f3547d46fb0398b16a6fc6dcb
harvey-sarah <101356948+harvey-sarah@users.noreply.github.com>
2022-05-04 15:01:53 -0400

    modified footer component to match design styling (#90)
    * modified footer component to match design styling
    
    * replaced number with variable

--- packages/ds/angular/src/stencil-generated/components.ts
+++ packages/ds/angular/src/stencil-generated/components.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* tslint:disable */
 /* auto-generated angular directive proxies */
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, NgZone } from '@angular/core';
 import { ProxyCmp, proxyOutputs } from './angular-component-lib/utils';
