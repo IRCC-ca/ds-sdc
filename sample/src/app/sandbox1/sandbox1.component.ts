97c9f96cb31fba7608038f1735d906ad059e9917
Hannah.Cass <92103482+cass-h@users.noreply.github.com>
2022-05-06 16:49:18 -0400

    added router and expanded sandbox, added dark/light to header

---
+++ sample/src/app/sandbox1/sandbox1.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'jds-sandbox1',
+    templateUrl: './sandbox1.component.html',
+    styleUrls: ['./sandbox1.component.scss'],
+})
+export class Sandbox1Component {}
