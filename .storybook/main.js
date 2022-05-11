414497ad072b2e3abe9fbd1c596acec08446b86d
Bun.Claire <claire.bun@cic.gc.ca>
2022-05-11 11:00:10 -0400

    Test documentations with stories.mdx in core pkg

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,14 @@
 module.exports = {
   "stories": [
+    "../packages/ds/core/**/*.stories.mdx",
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
+    '@storybook/addon-docs',  
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
   "framework": "@storybook/angular",
   "core": {
