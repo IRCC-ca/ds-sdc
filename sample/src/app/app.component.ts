71496adddd399a2a98affe6b9b8207703a193f8e
Bun.Claire <claire.bun@cic.gc.ca>
2022-05-12 16:40:12 -0400

    tests imports from ng pkg

--- sample/src/app/app.component.ts
+++ sample/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { IIconConfig } from '@ircc-ca/ds-sdc-angular';
 import { TranslateService } from '@ngx-translate/core';
 import { routes } from './routing/app.routing.module';
-// import { IFieldConfig } from 'packages/ds/angular/src/lib/jl-cl/jl-cl/IFormBase';
-
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -21,16 +18,6 @@ export class AppComponent {
     //   hint: 'test test',
     //   placeholder: '',
     // } as IFieldConfig;
-
-    alicornIconConfig = {
-        unicode: 'f6b0',
-        fontFamily: 'fa-solid',
-    } as IIconConfig;
-
-    rocketIconConfig = {
-        unicode: 'e027',
-        fontFamily: 'fa-solid',
-    } as IIconConfig;
 
     constructor(private translate: TranslateService, private router: Router) {}
 
--- sample/src/app/app.module.ts
+++ sample/src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { IRCCModule, JlCommonModule } from '@ircc-ca/ds-sdc-angular';
+import { IRCCModule } from '@ircc-ca/ds-sdc-angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { JLButtonModule } from '@ircc-ca/ds-sdc-angular/src/common/button';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './routing/app.routing.module';
 import { SandboxComponent } from './sandbox/sandbox.component';
 import { Sandbox1Component } from './sandbox1/sandbox1.component';
 import { ThemeSelectComponent } from './theme/theme.component';
+import { JLIconModule } from '@ircc-ca/ds-sdc-angular/src/common/icon';
+import { JLFooterModule } from '@ircc-ca/ds-sdc-angular/src/common/footer';
+import { JLHeaderModule } from '@ircc-ca/ds-sdc-angular/src/common/header';
+import { JLLinkModule } from '@ircc-ca/ds-sdc-angular/src/common/link';
+
+const JL_ANGULAR_COMPONENTS = [        
+    JLButtonModule,
+    JLIconModule,
+    JLLinkModule,
+    JLFooterModule,
+    JLHeaderModule
+]
 
 @NgModule({
     declarations: [AppComponent, SandboxComponent, Sandbox1Component, ThemeSelectComponent],
@@ -16,8 +29,8 @@ import { ThemeSelectComponent } from './theme/theme.component';
         FormsModule,
         ReactiveFormsModule,
         TranslateModule.forRoot(),
-        JlCommonModule,
         IRCCModule,
+        JL_ANGULAR_COMPONENTS,
         AppRoutingModule,
     ],
     providers: [],
