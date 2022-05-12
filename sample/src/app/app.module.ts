31884e8c471840403ce64a919cd89ab15ea7927b
Bun.Claire <claire.bun@cic.gc.ca>
2022-05-12 16:40:12 -0400

    tests imports from ng pkg

--- sample/src/app/app.module.ts
+++ sample/src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { IRCCModule, JlCommonModule } from '@ircc-ca/ds-sdc-angular';
+import { IRCCModule } from '@ircc-ca/ds-sdc-angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { JLButtonModule } from '@ircc-ca/ds-sdc-angular/src/common/button';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './routing/app.routing.module';
 import { SandboxComponent } from './sandbox/sandbox.component';
 import { ThemeSelectComponent } from './theme/theme.component';
+import { JLIconModule } from '@ircc-ca/ds-sdc-angular/src/common/icon';
+import { JLFooterModule } from '@ircc-ca/ds-sdc-angular/src/common/footer';
+import { JLHeaderModule } from '@ircc-ca/ds-sdc-angular/src/common/header';
+import { JLLinkModule } from '@ircc-ca/ds-sdc-angular/src/common/link';
+
+const JL_ANGULAR_COMPONENTS = [        
+    JLButtonModule,
+    JLIconModule,
+    JLLinkModule,
+    JLFooterModule,
+    JLHeaderModule
+]
 
 @NgModule({
     declarations: [AppComponent, SandboxComponent, ThemeSelectComponent],
@@ -15,8 +28,8 @@ import { ThemeSelectComponent } from './theme/theme.component';
         FormsModule,
         ReactiveFormsModule,
         TranslateModule.forRoot(),
-        JlCommonModule,
         IRCCModule,
+        JL_ANGULAR_COMPONENTS,
         AppRoutingModule,
     ],
     providers: [],
