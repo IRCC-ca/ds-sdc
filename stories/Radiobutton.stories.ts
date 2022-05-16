e3cbc539d16f5378bfabf9bad3ea1a9e3d4e6775
Bun.Claire <claire.bun@cic.gc.ca>
2022-05-16 11:04:30 -0400

    Add figma link to radio button sb

--- stories/Radiobutton.stories.ts
+++ stories/Radiobutton.stories.ts
@@ -2,8 +2,14 @@ import { Story, Meta } from '@storybook/angular/types-6-0';
 import { RadioButtonComponent } from '@ircc-ca/ds-sdc-angular';
 
 export default {
-  title: 'Radio button',
+  title: 'Components/Radio button',
   component: RadioButtonComponent,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/16zKKXc4pzBdRAXmbz6Ht4/Components?node-id=175%3A3907'
+    }
+  }
 } as Meta<RadioButtonComponent>;
 
 const Template: Story<RadioButtonComponent> = (args: RadioButtonComponent) => ({
