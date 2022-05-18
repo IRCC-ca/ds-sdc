ef038429fc37208ae063a669ea14132d8911a885
Bun.Claire <claire.bun@cic.gc.ca>
2022-05-18 11:03:22 -0400

    Update storybook docs and stories

---
+++ stories/Checkbox.stories.ts
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { CheckboxComponent } from '@ircc-ca/ds-sdc-angular';
+
+export default {
+  title: 'Components/Checkbox',
+  component: CheckboxComponent,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/16zKKXc4pzBdRAXmbz6Ht4/Components?node-id=175%3A3907'
+    }
+  }
+} as Meta<CheckboxComponent>;
+
+const Template: Story<CheckboxComponent> = (args: CheckboxComponent) => ({
+  props: {...args},
+  template: `<jds-checkbox>Angular package checkbox</jds-checkbox>`
+});
+
+export const Default = Template.bind({});
+
