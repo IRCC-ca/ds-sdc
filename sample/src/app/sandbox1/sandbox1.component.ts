8a7e535003e5ae63bea6f79d7abdfb3855d82c5d
Claire <claire.bun@cic.gc.ca>
2022-05-25 13:40:08 -0400

    fix bugs and size for checked color

--- sample/src/app/sandbox1/sandbox1.component.ts
+++ sample/src/app/sandbox1/sandbox1.component.ts
@@ -7,9 +7,16 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class Sandbox1Component implements AfterViewInit {
     @ViewChild('invalidRadio') invalidRadio!: ElementRef<HTMLInputElement>;
+    @ViewChild('invalidCheckedRadio')
+    invalidCheckedRadio!: ElementRef<HTMLInputElement>;
 
     ngAfterViewInit() {
         // set the radio button as invalid
         this.invalidRadio.nativeElement.setCustomValidity('invalid field');
+
+        // set radio as invalid and checked
+        this.invalidCheckedRadio.nativeElement.setCustomValidity(
+            'invalid field'
+        );
     }
 }
