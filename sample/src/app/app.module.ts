0d53854f03c0e2906c3c135e471181f7c0228e53
Claire Bun <104525407+claire-bun@users.noreply.github.com>
2022-05-26 13:49:49 -0400

    Sample App add ngx-translate (#119)
    * add ngx-translate lib to sample app
    
    * Add more trnaslation keys
    
    * update package-lock with new pkg

--- sample/src/app/app.module.ts
+++ sample/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,21 +9,31 @@ import { JLHeaderModule } from '@ircc-ca/ds-sdc-angular/header';
 import { JLIconModule } from '@ircc-ca/ds-sdc-angular/icon';
 import { JLLinkModule } from '@ircc-ca/ds-sdc-angular/link';
 import { JLRadiobuttonModule } from '@ircc-ca/ds-sdc-angular/radio-button';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './routing/app.routing.module';
 import { SandboxComponent } from './sandbox/sandbox.component';
 import { Sandbox1Component } from './sandbox1/sandbox1.component';
 import { ThemeSelectComponent } from './theme/theme.component';
 
-const JL_ANGULAR_COMPONENTS = [        
+const JL_ANGULAR_COMPONENTS = [
     JLButtonModule,
     JLIconModule,
     JLLinkModule,
     JLRadiobuttonModule,
     JLFooterModule,
-    JLHeaderModule
-]
+    JLHeaderModule,
+];
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+    return new TranslateHttpLoader(
+        httpClient,
+        './assets/translations/',
+        '.json'
+    );
+}
 
 @NgModule({
     declarations: [
@@ -34,8 +45,15 @@ const JL_ANGULAR_COMPONENTS = [
     imports: [
         BrowserModule,
         FormsModule,
+        HttpClientModule,
         ReactiveFormsModule,
-        TranslateModule.forRoot(),
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient],
+            },
+        }),
         IRCCModule,
         JL_ANGULAR_COMPONENTS,
         AppRoutingModule,
