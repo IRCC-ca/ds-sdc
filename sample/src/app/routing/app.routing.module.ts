16069670d30f0fca73c6085165c4455840745377
Sarah Harvey <101356948+harvey-sarah@users.noreply.github.com>
2022-06-08 10:50:48 -0400

    added styling for basic text inputs

--- sample/src/app/routing/app.routing.module.ts
+++ sample/src/app/routing/app.routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; // CLI imports router
 import { SandboxComponent } from '../sandbox/sandbox.component';
 import { Sandbox1Component } from '../sandbox1/sandbox1.component';
+import { Sandbox2Component } from '../sandbox2/sandbox2.component';
 export const routes: Routes = [
     { path: 'sandbox', component: SandboxComponent },
     { path: 'sandbox1', component: Sandbox1Component },
+    { path: 'sandbox2', component: Sandbox2Component },
 ];
 
 // configures NgModule imports and exports
