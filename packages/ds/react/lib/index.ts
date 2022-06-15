d16f18d1eb11860029957ed067cb295f404f27d9
Claire <claire.bun@cic.gc.ca>
2022-06-15 14:43:38 -0400

    generate stencil react output

---
+++ packages/ds/react/lib/index.ts
@@ -0,0 +1,5 @@
+import { defineCustomElements } from '@ircc-ca/ds-sdc-web/loader';
+
+export * from './stencil-generated/components';
+
+defineCustomElements();
---
+++ packages/ds/react/lib/stencil-generated/components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@ircc-ca/ds-sdc-web';
+
+
+
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
+export const WebButton = /*@__PURE__*/createReactComponent<JSX.WebButton, HTMLWebButtonElement>('web-button');
