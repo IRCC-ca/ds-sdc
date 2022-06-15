aae486f0cc0f34b16779ba210f361e28d7c0f2a1
Claire <claire.bun@cic.gc.ca>
2022-06-15 14:44:59 -0400

    generate react example projet

---
+++ examples/react-stencil/src/App.tsx
@@ -0,0 +1,12 @@
+import { MyComponent, WebButton } from '@ircc-ca/ds-sdc-react';
+
+function App() {
+    return (
+        <div>
+            <MyComponent></MyComponent>
+            <WebButton></WebButton>
+        </div>
+    );
+}
+
+export default App;
