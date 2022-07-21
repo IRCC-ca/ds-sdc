5bfbf9e2c1e950d180a5a7d1e0afb1e2648925c8
Michael.Jalowiecki <Michael.Jalowiecki@cic.gc.ca>
2022-07-21 14:20:13 -0400

    added file and search to input type

--- sample/src/app/routing/app.routing.module.ts
+++ sample/src/app/routing/app.routing.module.ts
@@ -4,11 +4,14 @@ import { SandboxStencilComponent } from '../sandbox-stencil/sandbox-stencil.comp
 import { SandboxComponent } from '../sandbox/sandbox.component';
 import { Sandbox1Component } from '../sandbox1/sandbox1.component';
 import { Sandbox2Component } from '../sandbox2/sandbox2.component';
+import { Sandbox3Component } from '../sandbox3/sandbox3.component';
+
 export const routes: Routes = [
     { path: 'sandbox', component: SandboxComponent },
     { path: 'sandbox-stencil', component: SandboxStencilComponent },
     { path: 'sandbox1', component: Sandbox1Component },
     { path: 'sandbox2', component: Sandbox2Component },
+    { path: 'sandbox3', component: Sandbox3Component },
 ];
 
 // configures NgModule imports and exports
