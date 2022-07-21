d8cf8de3e7cddd97de2522264e9767fc2b8033c2
MikeJalow <93152578+MikeJalow@users.noreply.github.com>
2022-07-21 14:43:50 -0400

    159 core form elements text input (#171)
    * added file and search to input type
    
    * changed comment

---
+++ sample/src/app/sandbox3/sandbox3.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'jds-sandbox3',
+    templateUrl: './sandbox3.component.html',
+    styleUrls: ['./sandbox3.component.scss'],
+})
+export class Sandbox3Component {}
