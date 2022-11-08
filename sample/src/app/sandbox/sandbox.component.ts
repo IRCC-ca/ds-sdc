e35bcc3a3cd9339ef338c3d94f058365c33169be
MikeJalow <93152578+MikeJalow@users.noreply.github.com>
2022-11-08 07:45:57 -0500

    Feature/color bugfixes mike (#265)
    * inital commit
    
    * bug fixes

--- sample/src/app/sandbox/sandbox.component.ts
+++ sample/src/app/sandbox/sandbox.component.ts
@@ -7,6 +7,7 @@ import { IIconConfig } from '@ircc-ca/ds-sdc-angular/icon';
     styleUrls: ['./sandbox.component.scss'],
 })
 export class SandboxComponent {
+
     rocketIconConfig = {
         unicode: 'e027',
         fontFamily: 'fa-solid',
