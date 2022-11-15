2334ed1ae9701ed600ba1290e33583eba3293c1f
Michael.Jalowiecki <Michael.Jalowiecki@cic.gc.ca>
2022-11-15 08:22:48 -0500

    sample proj setup

--- sample/src/app/sandbox3/sandbox3.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'jds-sandbox3',
-    templateUrl: './sandbox3.component.html',
-    styleUrls: ['./sandbox3.component.scss'],
-})
-export class Sandbox3Component {}
