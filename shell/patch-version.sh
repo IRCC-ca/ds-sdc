b29226628cd3b9a56c21ba7e693345e016543b00
MikeJalow <93152578+MikeJalow@users.noreply.github.com>
2022-12-06 10:35:13 -0500

    Feature/version updates mike (#365)
    * version changes
    
    * 0.0.3
    
    * updating package-json commands
    
    * 0.1.0

---
+++ shell/patch-version.sh
@@ -0,0 +1,25 @@
+npm version patch
+npm i
+cd packages/ds/core
+npm version patch
+npm i
+cd ../web
+npm version patch
+npm i
+cd ../angular
+npm version patch
+npm i
+cd ../react
+npm version patch
+npm i
+cd ../../..
+git add package-lock.json
+git add package.json
+git add packages/ds/angular/package-lock.json
+git add packages/ds/angular/package.json
+git add packages/ds/core/package-lock.json
+git add packages/ds/core/package.json
+git add packages/ds/react/package-lock.json
+git add packages/ds/react/package.json
+git add packages/ds/web/package-lock.json
+git add packages/ds/web/package.json
