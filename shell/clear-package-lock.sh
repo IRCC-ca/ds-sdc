52aea738a264412a74bd0c843d2c8422988f8e70
Michael.Jalowiecki <Michael.Jalowiecki@cic.gc.ca>
2022-12-12 15:18:44 -0500

    fixing dependency conflicts

---
+++ shell/clear-package-lock.sh
@@ -0,0 +1,12 @@
+cd packages/ds/core
+rm -f package-lock.json
+rm -r node_modules
+cd ../web
+rm -f package-lock.json
+rm -r node_modules
+cd ../angular
+rm -f package-lock.json
+rm -r node_modules
+cd ../react
+rm -f package-lock.json
+rm -r node_modules
