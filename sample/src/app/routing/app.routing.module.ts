89b5101a038e8aad11c9240bd62a4dd9516097e5
MikeJalow <93152578+MikeJalow@users.noreply.github.com>
2022-12-28 14:31:50 -0500

    Develop (#388)
    * removing stencil (#382)
    
    * removing stencil
    
    * renaming publish patch
    
    * Header: fix img not showing up issue in doc site (#381)
    
    * Header img fix patch v (#383)
    
    * Header: fix img not showing up issue in doc site
    
    * 0.1.2
    
    * 0.1.3
    
    * Header img fix patch v
    
    * workflow updates (#387)
    
    Co-authored-by: Xingruo (Michael) Xi <111757720+xingruoxicic@users.noreply.github.com>

--- sample/src/app/routing/app.routing.module.ts
+++ sample/src/app/routing/app.routing.module.ts
@@ -4,14 +4,14 @@ import { ColoursComponent } from '../dev/colours/colours.component';
 import { DevBlankComponent } from '../dev/dev-blank/dev-blank.component';
 import { HomeComponent } from '../home/home.component';
 import { InstructionsComponent } from '../instructions/instructions.component';
+import { AngularQAComponent } from '../qa/component-qa/angular-qa.component';
 import { CoreQAComponent } from '../qa/core-qa/core-qa.component';
 import { QABlankComponent } from '../qa/qa-blank/qa-blank.component';
-import { StencilQAComponent } from '../qa/stencil-qa/stencil-qa.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'instructions', component: InstructionsComponent },
-    { path: 'stencil-qa', component: StencilQAComponent },
+    { path: 'component-qa', component: AngularQAComponent },
     { path: 'qa-blank', component: QABlankComponent },
     { path: 'dev-blank', component: DevBlankComponent },
     { path: 'colours', component: ColoursComponent },
---
+++ shell/core-patch-version.sh
@@ -0,0 +1,6 @@
+cd ../core
+npm version patch
+npm i
+cd ../../..
+git add packages/ds/core/package-lock.json
+git add packages/ds/core/package.json
